e29ab15c434e57ccf26eb26ade306972873d5615
aymen braiek <aymen.braiek@esprit.tn>
2021-03-20 15:37:21 +0100

    Spring MVC @RequestBody Annotation -closes #5

--- src/main/java/biat/springframework/msscbreweryv2/Web/Controller/BeerController.java
+++ src/main/java/biat/springframework/msscbreweryv2/Web/Controller/BeerController.java
@@ -24,7 +24,7 @@ public class BeerController {
     }
 
     @PostMapping // POST - create new beer
-    public ResponseEntity handlePost( BeerDto beerDto){
+    public ResponseEntity handlePost( @RequestBody  BeerDto beerDto){
 
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
@@ -37,7 +37,7 @@ public class BeerController {
 
     /* Update beer */
     @PutMapping("/{beerId}")
-    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto) {
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId,@RequestBody BeerDto beerDto) {
         beerService.updatebeerId(beerId, beerDto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
